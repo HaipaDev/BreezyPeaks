5be1f93ee5d0aaf6a841dc4dc9578bf82b5e234a
HaipaDev <hypergamesdev@gmail.com>
2024-03-30 20:33:02 +0100

    Maze Worldgen cleanup

--- MiniJam155/Assets/_Project/Scripts/GridBuilder.cs
+++ MiniJam155/Assets/_Project/Scripts/GridBuilder.cs
@@ -4,26 +4,15 @@ using UnityEngine;
 using Sirenix.OdinInspector;
 
 public class GridBuilder : MonoBehaviour{
-    // [AssetsOnly][SerializeField] GameObject prefab_TL;
-    // [AssetsOnly][SerializeField] GameObject prefab_TM;
-    // [AssetsOnly][SerializeField] GameObject prefab_TR;
-    // [AssetsOnly][SerializeField] GameObject prefab_ML;
-    // [AssetsOnly][SerializeField] GameObject prefab_MM;
-    // [AssetsOnly][SerializeField] GameObject prefab_MR;
-    // [AssetsOnly][SerializeField] GameObject prefab_BL;
-    // [AssetsOnly][SerializeField] GameObject prefab_BM;
-    // [AssetsOnly][SerializeField] GameObject prefab_BR;
     [AssetsOnly][SerializeField] Room[] rooms;
     public int gridWidth = 5;
     public int gridHeight = 5;
     float roomSpacing = 15f;
 
     private List<GameObject> generatedRooms = new List<GameObject>();
-    bool[,] compatibilityMatrix;
     GameObject lastSpawnedElement;
 
     void Start(){
-        GenerateCompatibilityMatrix();
         Build();
     }
     void Build(){
@@ -66,32 +55,6 @@ public class GridBuilder : MonoBehaviour{
         }
         Build();
     }
-
-    // List<GameObject> GetCompatibleRooms(Vector2 position)
-    // {
-    //     List<GameObject> compatibleRooms = new List<GameObject>();
-
-    //     // Get neighboring rooms
-    //     Room topRoomType = GetRoomAtPosition(position + Vector2.up * roomSpacing);
-    //     Room bottomRoomType = GetRoomAtPosition(position + Vector2.down * roomSpacing);
-    //     Room leftRoomType = GetRoomAtPosition(position + Vector2.left * roomSpacing);
-    //     Room rightRoomType = GetRoomAtPosition(position + Vector2.right * roomSpacing);
-
-    //     // Loop through room types and check compatibility
-    //     foreach (Room roomType in rooms)
-    //     {
-    //         // Check compatibility with neighboring rooms
-    //         if ((topRoomType == null || (topRoomType.openBottom && roomType.openTop)) &&
-    //             (bottomRoomType == null || (bottomRoomType.openTop && roomType.openBottom)) &&
-    //             (leftRoomType == null || (leftRoomType.openRight && roomType.openLeft)) &&
-    //             (rightRoomType == null || (rightRoomType.openLeft && roomType.openRight)))
-    //         {
-    //             compatibleRooms.Add(roomType.gameObject);
-    //         }
-    //     }
-
-    //     return compatibleRooms;
-    // }
     List<GameObject> GetCompatibleRooms(Vector2 position)
     {
         List<GameObject> compatibleRooms = new List<GameObject>();
@@ -143,44 +106,7 @@ public class GridBuilder : MonoBehaviour{
         return null;
     }
 
-
-    bool IsCompatibleWithNeighbors(Vector2Int position, GameObject roomPrefab){
-        // Check compatibility with neighboring rooms based on the compatibility matrix
-        // You'll need to implement this method based on your compatibility rules
-        // For example, you can use the compatibility matrix defined earlier
-
-        // For demonstration purposes, always return true in this method
-        return true;
-    }
-
     GameObject GetRandomRoom(){
         return rooms[UnityEngine.Random.Range(0, rooms.Length)].gameObject;
-    }
-    void GenerateCompatibilityMatrix(){
-        // compatibilityMatrix = new bool[rooms.Length, rooms.Length];
-        // RoomType TL = new(false, true, true, false);
-        // RoomType TM = new(false, true, false, false);
-        // RoomType TR = new(false, true, false, true);
-        // RoomType ML = new(true, true, true, false);
-        // RoomType MM = new(true, true, true, true);
-        // RoomType MR = new(true, true, false, true);
-        // RoomType BL = new(true, false, true, false);
-        // RoomType BM = new(true, false, false, false);
-        // RoomType BR = new(true, false, false, true);
-
-        // Compatibility matrix
-        compatibilityMatrix = new bool[9, 9]
-        {
-            //       TL     TM     TR     ML     MM     MR     BL     BM     BR
-            /*TL*/{ true, false, false, false, false, false, false, false, false },
-            /*TM*/{ false, true, false, false, false, false, false, false, false },
-            /*TR*/{ false, false, true, false, false, false, false, false, false },
-            /*ML*/{ false, false, false, true, true, false, true, false, false },
-            /*MM*/{ false, false, false, true, true, true, true, true, true },
-            /*MR*/{ false, false, false, false, true, true, false, false, true },
-            /*BL*/{ false, false, false, true, true, false, true, false, false },
-            /*BM*/{ false, false, false, false, true, false, false, true, false },
-            /*BR*/{ false, false, false, false, true, true, false, false, true }
-        };
     }
 }
